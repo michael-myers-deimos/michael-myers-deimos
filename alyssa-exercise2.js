f74d53e611011a31b31eb88ea2684cba29d1c172
alyssalanae <briggs.l.alyssa@gmail.com>
2019-10-23 15:22:28 -0500

    Completed exercise 2 with harder problem

--- alyssa-exercise2.js
+++ alyssa-exercise2.js
@@ -1,31 +1,56 @@
 "use strict";
 
 // https://java.codeup.com/extra-exercises/javascript/arrays-and-objects/
-//Exercise 2 from this website ^^
-// Write a function, getOlder() that takes in array of dog objects and increases the value of each object's age property by 1.
-var dogs = [
+//Exercise 4 from this website ^^
+// Write a function, adminList() that takes in an array of user objects and returns a count of all admins based on the isAdmin property. Refactor to return an array of admin-only user emails. Refactor again to return an array of user objects that are admins.
+var admins = [
     {
-        name: "Chompers",
-        breed: "Pug",
-        age: 7
+        isAdmin: true,
+        email: 'user1@email.com'
     },
     {
-        name: "Freddy",
-        breed: "Lab",
-        age: 4
+        isAdmin: true,
+        email: 'user2@email.com'
     },
     {
-        name: "Mr. Pig",
-        breed: "Mastif",
-        age: 10
+        isAdmin: false,
+        email: 'user3@email.com'
     }
 ];
 
-function getOlder(){
-    for (var i = 0; i< dogs.length; i++){
-        dogs[i].age++
+function adminList(){
+    var countAdmins = 0;
+    for (var i = 0; i< admins.length; i++){
+        if (admins[i].isAdmin === true){
+            countAdmins++
+        }
     }
-    return dogs;
+    return countAdmins;
 }
 
-console.log(getOlder());
+console.log(adminList());
+
+function adminEmail(){
+    var email = [];
+    for (var i = 0; i < admins.length; i++){
+        if (admins[i].isAdmin === true){
+            email.push(admins[i].email)
+        }
+    }
+        return email;
+}
+
+console.log(adminEmail());
+
+
+function isAdmin(){
+    var adminObject = [];
+    for (var i = 0; i < admins.length; i++){
+        if (admins[i].isAdmin === true){
+            adminObject.push(admins[i])
+        }
+    }
+    return adminObject;
+}
+
+console.log(isAdmin());
