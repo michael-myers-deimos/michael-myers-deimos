695071af0e2f21bdb3b480f5829d1cb71de55be3
MelindaGreene <Melindagreene711@gmail.com>
2019-10-25 15:20:58 -0500

    melinda

--- Melinda-Excercise 1.js
+++ Melinda-Excercise 1.js
@@ -22,47 +22,114 @@
 //     my dog has shot records for a "rabies shot, a lyme diesease shot and flea shot"  all dated the same day 10/20/2018.
 //
 
-const dog =
-    {
-        breed: ["Pitbull"],
-        weightInPounds: "45lbs",
-        age: 10,
-        color: "grey",
-        sterilized: false,
-        shotRecords: {
-            typeOfShot: "Rabies",
-            date: new Date(),
+const dogs =
+    [
+        {
+            breed: ["Pitbull"],
+            weightInPounds: "45lbs",
+            age:  10,
+            color: "Grey",
+            sterilized: true,
+            shotRecords: {
+                typeOfShot: "& Rabies",
+                date: new Date(),
+            },
         },
-    };
+        {
+            breed: ["Beagle"],
+            weightInPounds: "30lbs",
+            age: 5,
+            color: "Brown and white",
+            sterilized: false,
+            shotRecords: {
+                typeOfShot: " & Lyme ",
+                date: new Date(),
 
-dog.bark =function (){
+            },
+        },
+        {
+            breed: ["Pug"],
+            weightInPounds: "20lbs",
+            age: 2,
+            color: "Brown and White",
+            sterilized: false,
+            shotRecords: {
+                typeOfShot: " & Canine Distemper",
+                date: new Date(),
+
+            },
+            getOlder: function () {
+                for (var i = 0; i < dogs.length; i++) {
+                    dogs[i].age++;
+                }
+            }
+        }
+    ];
+// var dog= dogs[0];
+var dog = {
+    getOlder: function () {
+        for (var i = 0; i < dogs.length; i++) {
+            dogs[i].age++;
+        }
+    },
+
+    bark: function() {
     console.log("Woof!");
+    },
+
+    fix: function () {
+        for(var i = 0; i < dogs.length; i++) {
+            if (dogs[i].sterilized === false) {
+                return true;
+            } else {
+                return false
+            }
+        }
+
+    },
+
+    vaccinate: function () {
+        for(var i = 0; i < dogs.length; i++){
+            return dogs[i].shotRecords.typeOfShot + " and  Flea/Tick " + " on " + dogs[i].shotRecords.date;
+        }
+
+
+}
 };
-
-dog.getOlder = function(){
-    // return x + 1;
-    return dog.age + 1;
-};
-
-dog.fix = function(sterile){
-    if( sterile === true);
-    return false;
-};
-
-dog.vaccinate = function(){
-    return  dog.shotRecords.typeOfShot + " and  Flea/Tick " + " on " + dog.shotRecords.date;
-
-};
-
-   console.log(" My dog is a " + dog.breed + " and weighs " + dog.weightInPounds + "." +" My " + dog.breed + "s" + " age is  " + dog.getOlder() + ". "
-   + "My dog has been vaccinated for " + dog.shotRecords.typeOfShot + dog.vaccinate() + ". " +  dog.bark());
+    for (let i = [0]; i < dogs.length ; i++){
+   console.log(" My dog is a " + dogs[i].breed + " and weighs " + dogs[i].weightInPounds + "." +" My " + dogs[i].breed + "s" + " age is  " +  dog.getOlder(dogs[0].age) + ". "
+   + "My dog has been vaccinated for " + dogs[i].shotRecords.typeOfShot + dog.vaccinate(dogs[i]) + ". " + " My dog is the color " + dogs[i].color + " and has been fixed " + dog.fix(dogs[i]) + dog.bark(dogs[i]) );
+    }
 
 
+// 2nd Exercise
+// Write a JavaScript program where the program takes a random integer between 1 to 10, the user is then prompted to input a guess number. If the user input matches with guess number, the program will display a message "Good Work" otherwise display a message "Not matched"
+//
+
+var Randonum = Math.ceil(Math.random() * 40);
+console.log(Randonum);
+var number = prompt(" Guess a number between 1 and 40");
+if (Randonum == number)
+    alert("Great Job!!");
+else
+    alert("Looks like you cant read the computers mind the number was " + Randonum);
 
 
+// 3rd Exercise
+
+//
+// Write a JavaScript program to create a new string from a given string with the first character of the given string added at the front and back.
+//
 
 
+// Substring is a inbuilt function in javascript used to return a  part of string from start of index to end of index
 
-
-
+    function moveToFrontEnd(str)
+{
+    var first = str.substring(0,1);
+    return first + str + first;
+}
+console.log(moveToFrontEnd('tired'));
+console.log(moveToFrontEnd('anxious'));
+console.log(moveToFrontEnd('hungry'));
 
