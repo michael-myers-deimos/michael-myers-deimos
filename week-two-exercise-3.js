6d17d0274676f1a8f672ba1f7a1fd68b49120a5e
ChristianCrousser <christian.crousserkaiman@gmail.com>
2019-11-01 09:33:08 -0500

    All exercises complete

---
+++ week-two-exercise-3.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var myFavoriteGames = ["Halo 3", "LoZ: Wind Waker", "Super Monkey Ball", "Castle Crashers", "Super Smash Bros.", "Gauntlet Dark Legacy", "Horizon Zero Dawn", "Tetris", "Trials Evolution", "Jak 3"];
+var elective = myFavoriteGames[Math.floor(Math.random() * myFavoriteGames.length)];
+alert(elective);
