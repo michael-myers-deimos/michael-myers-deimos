b3d5647fd46c3710c3162fb92886bc9f434ca1b5
ChristianCrousser <christian.crousserkaiman@gmail.com>
2019-11-01 12:15:27 -0500

    changed some variables

--- week-two-exercise-3.js
+++ week-two-exercise-3.js
@@ -1,5 +1,9 @@
 "use strict";
 
+
+// variable with an array of games
 var myFavoriteGames = ["Halo 3", "LoZ: Wind Waker", "Super Monkey Ball", "Castle Crashers", "Super Smash Bros.", "Gauntlet Dark Legacy", "Horizon Zero Dawn", "Tetris", "Trials Evolution", "Jak 3"];
-var elective = myFavoriteGames[Math.floor(Math.random() * myFavoriteGames.length)];
-alert(elective);
+
+// chooses a random game
+var games = myFavoriteGames[Math.floor(Math.random() * myFavoriteGames.length)];
+alert(games);
